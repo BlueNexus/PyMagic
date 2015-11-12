3109ff30cdbb4b38384109828eb39bba9bcd14a6
BlueNexus <benreeve99@yahoo.co.uk>
2015-11-12 17:36:24 +0000

    Fixed list parsing. Added debugging output

---
+++ PyMagic/PyMagic/PyMagic.py
@@ -0,0 +1,5 @@
+import classCards
+import classPlayer
+
+print(classCards.allCards)
+print(input("Please enter a word to confirm."))
--- PyMagic/PyMagic/classCards.py
+++ PyMagic/PyMagic/classCards.py
@@ -1,31 +1,5 @@
 ﻿import csv
 
-allCards = []
-with open('monsterList.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        cache = 0
-        IDname = ""
-        name = ""
-        cost = 0
-        power = 0
-        defence = 0
-        ability = 0
-        for word in row:
-            if cache == 0:
-                name = word
-            elif cache == 1:
-                IDname = word
-            elif cache == 2:
-                cost = word
-            elif cache == 3:
-                power = word
-            elif cache == 4:
-                defence = word
-            elif cache == 5:
-                ability = word
-            cache += 1
-        IDname = Monster(name, cost, power, defence, ability)
 
 class Card(object):
     def __init__(self, name, cost):
@@ -41,6 +15,9 @@ class Monster(Card):
         self.defence = defence
         self.health = defence
         self.ability = ability
+
+    def __repr__(self):
+        return "Monster: " + self.name + " H: " + self.health + " C: " + self.cost + " P: " + self.power + " D: " + self.defence + " A: " + self.ability
     
     def HealthCheck(self):
         if self.health < 1:
@@ -88,3 +65,32 @@ class Monster(Card):
             target.hit(self, False)
             target.HealthCheck()
             self.HealthCheck()
+
+
+allCards = []
+with open('monsterList.csv', 'rt') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        cache = 0
+        IDname = ""
+        name = ""
+        cost = 0
+        power = 0
+        defence = 0
+        ability = 0
+        for word in row:
+            if cache == 0:
+                name = word
+            elif cache == 1:
+                cost = word
+            elif cache == 2:
+                power = word
+            elif cache == 3:
+                defence = word
+            elif cache == 4:
+                ability = word
+            elif cache == 5:
+                IDname = word
+            cache += 1
+        IDname = Monster(name, cost, power, defence, ability)
+        allCards.append(IDname)
