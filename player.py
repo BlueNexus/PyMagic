3b5ed3802057f5fac61e50df0a0b8181b7a05425
BlueNexus <benreeve99@yahoo.co.uk>
2015-11-12 14:54:49 +0000

    Create player.py
    This is just the basis for now. I'll add more once everything else is ready.

---
+++ player.py
@@ -0,0 +1,29 @@
+class player(object):
+  hand = []
+  deck = []
+  field = []
+  grave = []
+  lands
+  health = 0
+  mana = 0
+  def __init__(self):
+    self.initDeck()
+    self.health = 20
+    for i in range(1, 8):
+      self.draw()
+    
+  
+  def draw(self):
+    self.hand.append(self.deck[(len(self.deck) - 1)]
+    del self.deck[(len(self.deck) - 1)]
+    
+  def startTurn(self):
+    if len(self.hand) < 8:
+      self.draw()
+    for i in self.lands:
+      self.mana += i.mana
+  
+  def endTurn(self):
+    for m in self.field:
+      m.health = m.defence
+      m.untap()
