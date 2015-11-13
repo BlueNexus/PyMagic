79231e60869e1c51f599f9b9488b34b5e4dc5ed7
BlueNexus <benreeve99@yahoo.co.uk>
2015-11-13 11:34:32 +0000

    Added summoning and SeeHand/field/etc

--- PyMagic/PyMagic/classPlayer.py
+++ PyMagic/PyMagic/classPlayer.py
@@ -1,4 +1,4 @@
-﻿import classCards
+import classCards
 import random
 
 class player(object):
@@ -10,12 +10,12 @@ class player(object):
     health = 0
     mana = 0
     def __init__(self):
-        self.initDeck()
+        self.InitDeck()
         self.health = 20
         for i in range(1, 8):
             self.draw()
     
-    def initDeck(self):
+    def InitDeck(self):
         for i in range(1, 61):
             self.deck.append(classCards.allCards[random.randrange(len(classCards.allCards))])
                 
@@ -24,13 +24,42 @@ class player(object):
         self.hand.append(self.deck[(len(self.deck) - 1)])
         del self.deck[len(self.deck) - 1]
 
-    def startTurn(self):
+    def CanSummon(self, target):
+        if target in self.hand and target.cost <= self.mana:
+            return True
+        else:
+            return False
+
+    def Summon(self, target):
+        if self.CanSummon(target) == True:
+            self.field.append(target)
+            del self.hand[target]
+            self.mana -= target.cost
+            print(self + " summons a " + target.name + "!")
+
+    def SeeHand(self):
+        for card in self.hand:
+            print(card)
+
+    def SeeField(self):
+        for card in self.field:
+            print(card)
+
+    def SeeGrave(self):
+        for card in self.grave:
+            print(card)
+
+    def SeeLand(self):
+        for card in self.lands:
+            print(card)
+
+    def StartTurn(self):
         if len(self.hand) < 8:
             self.draw()
         for i in self.lands:
             self.mana += i.mana
   
-    def endTurn(self):
+    def EndTurn(self):
         for m in self.field:
             m.health = m.defence
             m.untap()
