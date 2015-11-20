18c7df4b81fbce174f19c313f9726ecf70ae5fd4
BlueNexus <benreeve99@yahoo.co.uk>
2015-11-20 14:12:07 +0000

    Cleaned up some excess code

--- PyMagic/PyMagic/CardGenerator.py
+++ PyMagic/PyMagic/CardGenerator.py
@@ -8,7 +8,6 @@ class CardGenerator(object):
     def __init__(self, maxPower, maxDefence, balancer, cardCount):
         self.InitNames()
         self.InitAbilities()
-        print("CardGen: Initialisation complete.")
         self.maxPower = maxPower
         self.maxDefence = maxDefence
         self.balancer = balancer
@@ -16,10 +15,8 @@ class CardGenerator(object):
         self.Generate()
 
     def Generate(self):
-        print("CardGen: Generating " + self.cardCount + " cards.")
         workDeck = []
         for i in self.cardCount:
-            print("CardGen: Generating card ", i, " of ", self.cardCount)
             workName = self.nameList[randint(len(self.nameList))]
             workAbility = self.abilityList[randint(len(self.abilityList))]
             workPower = randint(self.maxPower)
@@ -28,22 +25,18 @@ class CardGenerator(object):
             workCost = int(((workPower + workDefence) / self.balancer) + (workAbility.cost / self.balancer))
             workID = Creature(workname, workCost, workPower, workDefence, workAbility, workID)
             workDeck.append(workID)
-        print("CardGen: Generation complete.")
         self.Save(workDeck)
 
     def Save(self, deck):
-        print("CardGen: Saving cardlist")
         file = open('CardGenerator\cardList.csv', 'a')
         for card in deck:
             file.write(card)
             file.write("\n")
-        print("CardGen: Saved ", len(deck), " cards.")
         file.close()
 
     def InitNames(self):
         with open('CardGenerator\nameList.csv', 'rt') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            print("CardGen: Initialising name list.")
             for row in spamreader:
                 name = ""
                 for word in row:
@@ -53,7 +46,6 @@ class CardGenerator(object):
     def InitAbilities(self):
         with open('CardGenerator\abilityList.csv', 'rt') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            print("CardGen: Initialising ability list.")
             for row in spamreader:
                 name = ""
                 cost = 0
@@ -66,34 +58,3 @@ class CardGenerator(object):
                         cost = word
                 name = Ability(name, cost)
                 abilityList.append(name)
-
-
-'''
-with open('monsterList.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        cache = 0
-        IDname = ""
-        name = ""
-        cost = 0
-        power = 0
-        defence = 0
-        ability = 0
-        for word in row:
-            if cache == 0:
-                name = word
-            elif cache == 1:
-                IDname = word
-            elif cache == 2:
-                cost = word
-            elif cache == 3:
-                power = word
-            elif cache == 4:
-                defence = word
-            elif cache == 5:
-                ability = word
-            cache += 1
-        IDname = Monster(name, cost, power, defence, ability)
-
-(In case I decide to go back to premade cards)
-'''
